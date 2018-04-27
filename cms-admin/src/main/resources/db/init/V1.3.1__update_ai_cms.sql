d8664d2c595b50aaa61271301fb6ec627b8cf579
冷伟平 <lengweiping@joyutech.com>
2018-04-27 18:05:46 +0800

    优化

--- cms-admin/src/main/resources/db/init/V1.3.1__update_ai_cms.sql
+++ cms-admin/src/main/resources/db/init/V1.3.1__update_ai_cms.sql
@@ -1,5 +1,8 @@
 use ai_cms;
 
+ALTER TABLE `cms_media_import` ADD COLUMN `audit_status` VARCHAR(255);
+ALTER TABLE `cms_media_import` ADD COLUMN `cp_code` VARCHAR(255);
+
 ALTER TABLE `cms_media_series` ADD COLUMN `receive_code` VARCHAR(255);
 ALTER TABLE `cms_media_program` ADD COLUMN `receive_code` VARCHAR(255);
 ALTER TABLE `cms_media_file` ADD COLUMN `receive_code` VARCHAR(255);
